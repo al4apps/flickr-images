8149b4698aae01c096bc681781d7a9e00b446434
Alisher Abdurashidov <abdurashidov.au@gmail.com>
2025-04-04 13:42:24 +0700

    [~] Комментарии

--- app/src/main/java/com/example/pix/data/room/PictureDatabase.kt
+++ app/src/main/java/com/example/pix/data/room/PictureDatabase.kt
@@ -3,6 +3,11 @@ package com.example.pix.data.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
+/**
+ * Так как в ТЗ нет указаний про работу с БД и никакой полезной логики с БД в рамках данного
+ * приложения я не придумал, БД не используется.
+ */
+
 @Database(entities = [PictureDbo::class], version = 1)
 abstract class PictureDatabase: RoomDatabase() {
     abstract fun getPictureDao(): PictureDao
